ec9298a6389ec673b7c22c91f9021413ad47f036
C.W. Betts <computers57@hotmail.com>
2024-10-13 15:40:03 -0600

    Remove unused C/ObjC headers. Minor info.plist poke.

--- EnvironmentPref.swift
+++ EnvironmentPref.swift
@@ -3,6 +3,28 @@
 //  RCEnvironment
 //
 //  Created by C.W. Betts on 10/13/24.
+//  All rights reserved.
+//
+//  Redistribution and use in source and binary forms, with or without
+//  modification, are permitted provided that the following conditions
+//  are met:
+//  1. Redistributions of source code must retain the above copyright
+//     notice, this list of conditions and the following disclaimer.
+//  2. Redistributions in binary form must reproduce the above copyright
+//     notice, this list of conditions and the following disclaimer in the
+//     documentation and/or other materials provided with the distribution.
+//
+//  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS IS''
+//  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+//  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+//  ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+//  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+//  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+//  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+//  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+//  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+//  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+//  POSSIBILITY OF SUCH DAMAGE.
 //
 
 import Cocoa
@@ -50,7 +72,7 @@ public class EnvironmentPref : NSPreferencePane {
 		
 		let infoDictionary = myBundle.infoDictionary!
 		keyValueDataSource.bundleIdentifier = infoDictionary["CFBundleIdentifier"] as? String
-		versionField.stringValue = infoDictionary["CFBundleVersion"] as? String ?? "1.0"
+		versionField.stringValue = infoDictionary["CFBundleVersion"] as? String ?? "unknown"
 		
 		if let aboutTextFile = myBundle.url(forResource: "AboutText", withExtension: "rtf") {
 			var aboutTextScrollView = aboutField.enclosingScrollView!
